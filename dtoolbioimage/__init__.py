c1545c46c052f11398371a31dd0c2828840c8b1f
Matthew Hartley <mhartley@cantab.net>
2018-12-08 20:54:08 +0000

    Add Jupyter integration

--- dtoolbioimage/__init__.py
+++ dtoolbioimage/__init__.py
@@ -1,16 +1,54 @@
+import os
 
+from io import BytesIO
 from collections import defaultdict
 
-from imageio import imread
+from imageio import imread, imsave, mimsave
 
 import numpy as np
 
 import dtoolcore
 
+
+class ImageMetadata(object):
+
+    def __init__(self, metadata_dict):
+        self.metadata_dict = metadata_dict
+
+    def __getattr__(self, name):
+        return self.metadata_dict[name]
+
+
+class Image3D(np.ndarray):
+
+    def _repr_png_(self):
+
+        if len(self.shape) == 2:
+            b = BytesIO()
+            imsave(b, self, 'PNG')
+
+            return b.getvalue()
+
+        return self[:, :,0]._repr_png_()
+
+    def save(self, fpath):
+        _, ext = os.path.splitext(fpath)
+        assert ext in ['.tif', '.tiff']
+
+        # We use row, col, z, but mimsave expects z, row, col
+        transposed = np.transpose(self, axes=[2, 0, 1])
+        mimsave(fpath, transposed)
+
+
+
 class ImageDataSet(object):
 
     def __init__(self, uri):
         self.dataset = dtoolcore.DataSet.from_uri(uri)
+
+        self.build_index()
+
+        self.metadata = self.dataset.get_overlay('microscope_metadata')
 
     def build_index(self):
         coords_overlay = self.dataset.get_overlay("plane_coords")
@@ -42,6 +80,25 @@ class ImageDataSet(object):
             for z in sorted(z_idns)
         ]
 
-        selected_planes = [im[:, :, channel] for im in images]
+        def safe_select_channel(im):
+            if len(im.shape) == 2:
+                return im
+            elif len(im.shape) == 3:
+                return im[:, :, channel]
+            else:
+                raise("Weird dimensions")
 
-        return np.dstack(selected_planes)
+        selected_planes = [safe_select_channel(im) for im in images]
+
+        stack = np.dstack(selected_planes).view(Image3D)
+        stack.metadata = ImageMetadata(self.metadata[z_idns[0]])
+
+        return stack
+
+    def get_image_names(self):
+        
+        return list(self.planes_index.keys())
+
+    def get_series_names(self, image_name):
+
+        return list(self.planes_index[image_name].keys())
