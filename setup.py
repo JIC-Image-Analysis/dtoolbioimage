ab35ac38090155bf81827255ee3df5923ca77870
Matthew Hartley <mhartley@cantab.net>
2018-12-09 21:30:03 +0000

    Update version

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
 
 setup(name='dtoolbioimage',
-      version='0.1.1',
+      version='0.1.2',
       description='dtool bioimaging utilties',
       url='https://github.com/JIC-Image-Analysis/dtoolbioimage',
       author='Matthew Hartley',
-      author_embioimagel='Matthew.Hartley@jic.ac.uk',
+      author_email='Matthew.Hartley@jic.ac.uk',
       license='MIT',
       packages=['dtoolbioimage'],
       install_requires=[
+	  "click",
+	  "parse",
           "imageio",
           "dtoolcore",
           "ipywidgets",
