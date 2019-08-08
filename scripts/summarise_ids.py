f2c20f77c9bffa915a5f5d6811b1d7b508a543d4
Matthew Hartley <mhartley@cantab.net>
2019-08-08 08:20:29 +0100

    Fix summary script for new internal format

--- scripts/summarise_ids.py
+++ scripts/summarise_ids.py
@@ -6,20 +6,22 @@ from dtoolbioimage import ImageDataSet
 def show_summary(image_dataset):
 
     def get_dimension_data(entry):
-        im_name, s_name = entry
-        channels = image_dataset.planes_index[im_name][s_name].keys()
+        im_name, s_name, sidx = entry
+        series = image_dataset.planes_index[im_name][s_name].keys()
+        n_series = len(image_dataset.planes_index[im_name][s_name])
+        channels = image_dataset.planes_index[im_name][s_name][sidx].keys()
         n_channels = len(channels)
-        n_planes = len(image_dataset.planes_index[im_name][s_name][0])
-        idn = image_dataset.planes_index[im_name][s_name][0][0]
+        n_planes = len(image_dataset.planes_index[im_name][s_name][sidx][0])
+        idn = image_dataset.planes_index[im_name][s_name][sidx][0][0]
         xdim = image_dataset.metadata[idn]['SizeX']
         ydim = image_dataset.metadata[idn]['SizeY']
 
-        return (n_channels, n_planes, xdim, ydim)
+        return (n_series, n_channels, n_planes, xdim, ydim)
 
-    for entry in image_dataset.iternames():
-        im_name, s_name = entry
-        n_channels, n_planes, xdim, ydim = get_dimension_data(entry)
-        print("{}, {}, {} channels, {}x{}x{}".format(im_name, s_name, n_channels, xdim, ydim, n_planes))
+    for entry in image_dataset.all_possible_stack_tuples():
+        im_name, s_name, sidx = entry
+        n_series, n_channels, n_planes, xdim, ydim = get_dimension_data(entry)
+        print("{}, {}, {} series, {} channels, {}x{}x{}".format(im_name, s_name, n_series, n_channels, xdim, ydim, n_planes))
 
 
 
