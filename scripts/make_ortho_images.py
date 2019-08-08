134c0fdade9413c2bbc8796a65dcfa8270f5de95
Matthew Hartley <mhartley@cantab.net>
2019-08-08 08:20:53 +0100

    Adapt orthogonal images script to write output to new dataset

--- scripts/make_ortho_images.py
+++ scripts/make_ortho_images.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from imageio import imsave
 from dtoolbioimage import ImageDataSet, zoom_to_match_scales
+from dtool_utils.derived_dataset import DerivedDataSet
 
 
 def make_cross_section_view(stack):
@@ -32,28 +33,34 @@ def make_cross_section_view(stack):
     return orthoview
 
 
-def make_cross_sections(image_ds):
+def make_cross_sections(image_ds, output_ds):
 
-    for im, sn in image_ds.iternames():
+    for im_name, sn in image_ds.iternames():
 
-        logging.info('Generating view for {}'.format(im))
-        stack = image_ds.get_stack(im, sn, channel=1)
+        logging.info('Generating view for {}'.format(im_name))
+        stack = image_ds.get_stack(im_name, sn, channel=1)
         stack = zoom_to_match_scales(stack)
 
         orthoview = make_cross_section_view(stack)
 
-        imsave('scratch/{}-orth.png'.format(im), orthoview)
+        relpath = f"{im_name}-orth.png"
+        fpath = output_ds.staging_fpath(relpath)
+        imsave(fpath, orthoview)
 
 
 
 @click.command()
 @click.argument('image_dataset_uri')
-def main(image_dataset_uri):
+@click.argument('output_base_uri')
+@click.argument('output_name')
+def main(image_dataset_uri, output_base_uri, output_name):
 
     logging.basicConfig(level=logging.INFO)
 
     image_ds = ImageDataSet(image_dataset_uri)
-    make_cross_sections(image_ds)
+
+    with DerivedDataSet(output_base_uri, output_name, image_ds) as output_ds:
+        make_cross_sections(image_ds, output_ds)
     
 
 if __name__ == "__main__":
